82f79b84fb802849b3a5d6924a97756c162676fd
Tatsukikun999 <59278386+Tatsukikun999@users.noreply.github.com>
2020-01-28 17:01:33 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(){
+    ifstream f1 ("cheerbook.txt");
+    ofstream f2 ("cheerbook_copy.txt");
+    string s;
+    f2 << "-------------------- SOTUS ---------------------\n";
+    while(getline(f1,s)){
+        f2 << s <<"\n";
+        }
+    f1.close();
+    f2 << "-------------------- SOTUS ---------------------";
+    f2.close();
+
+    
+}
 //cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
+// << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
