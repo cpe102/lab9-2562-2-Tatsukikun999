a811660a3432dcdf6d8cc3b0cff06d89445573f7
Tatsukikun999 <59278386+Tatsukikun999@users.noreply.github.com>
2020-01-28 17:02:09 +0700

    Update lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<cmath>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    ifstream in("score.txt");
+    double d,z,m,n,sd,sqd,sqs;
+    string s;
+    while(getline(in,s)){
+        //d = atof(s.c_str());
+        //z = z+d;
+
+    
+        z += atof(s.c_str());
+
+        n++;
+        //sqd = atof(s.c_str());
+        //sqs = sqs+pow(sqd,2);
+
+        sqs += pow(atof(s.c_str()),2);
+    } m = z/n;
+    sd = sqrt((1/n)*sqs-pow(m,2));
+
+cout << "Number of data = "<<n<<"\n";
+cout << "Mean = "<<m<<"\n";
+cout << "Standard deviation = "<<sd<<"\n";
+
+
+}
